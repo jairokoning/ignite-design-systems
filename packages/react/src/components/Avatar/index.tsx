0f55fcf79c2e96ceefaec6efa5563efae08662d3
Jairo Koning <jairo.koning@gmail.com>
2024-06-19 05:51:38 -0300

    feat: add avatar component

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,15 @@
+import { User } from '@phosphor-icons/react'
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+import { ComponentProps } from 'react'
+
+export type AvatarProps = ComponentProps<typeof AvatarImage>
+export function Avatar() {
+  return (
+    <AvatarContainer>
+      <AvatarImage />
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
